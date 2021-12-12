76bac9c6fdc4bf4bf8926ff8fa99074caa01e865
Dillon-Furtado <92597564+Dillon-Furtado@users.noreply.github.com>
2021-12-11 20:03:35 -0500

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -2,14 +2,108 @@ namespace SpriteKind {
     export const NPC = SpriteKind.create()
     export const Portal = SpriteKind.create()
 }
+sprites.onCreated(SpriteKind.Enemy, function (sprite) {
+    sprite.setPosition(135, 110)
+})
+controller.up.onEvent(ControllerButtonEvent.Pressed, function () {
+    if (hero.y > 110) {
+        hero.setImage(img`
+            . . . . . . f f f f . . . . . . 
+            . . . . f f f 2 2 f f f . . . . 
+            . . . f f f 2 2 2 2 f f f . . . 
+            . . f f f e e e e e e f f f . . 
+            . . f f e 2 2 2 2 2 2 e e f . . 
+            . . f e 2 f f f f f f 2 e f . . 
+            . . f f f f e e e e f f f f . . 
+            . f f e f b f 4 4 f b f e f f . 
+            . f e e 4 1 f d d f 1 4 e e f . 
+            . . f e e d d d d d d e e f . . 
+            . . . f e e 4 4 4 4 e e f . . . 
+            . . e 4 f 2 2 2 2 2 2 f 4 e . . 
+            . . 4 d f 2 2 2 2 2 2 f d 4 . . 
+            . . 4 4 f 4 4 5 5 4 4 f 4 4 . . 
+            . . . . . f f f f f f . . . . . 
+            . . . . . f f . . f f . . . . . 
+            `)
+        hero.vy = -70
+        canAttack = false
+        swordOut = false
+    }
+})
 controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
     canAttack = true
 })
 sprites.onOverlap(SpriteKind.Player, SpriteKind.Portal, function (sprite, otherSprite) {
     enemyScene()
     guide.destroy()
-    portal.destroy()
+    otherSprite.destroy()
 })
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Projectile, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+    pause(500)
+})
+function newEnemy () {
+    hero.x = 50
+    if (Math.percentChance(50)) {
+        ghost = sprites.create(img`
+            ........................
+            ........................
+            ........................
+            ........................
+            ..........ffff..........
+            ........ff1111ff........
+            .......fb111111bf.......
+            .......f1111111df.......
+            ......fd1111111ddf......
+            ......fd111111dddf......
+            ......fd111ddddddf......
+            ......fd1dfbddddbf......
+            ......fbddfcdbbbcf......
+            .......f11111bbcf.......
+            .......f1b1fffff........
+            .......fbfc111bf........
+            ........ff1b1bff........
+            .........fbfbfff.f......
+            ..........ffffffff......
+            ............fffff.......
+            ........................
+            ........................
+            ........................
+            ........................
+            `, SpriteKind.Enemy)
+        fightGhost = true
+        fightDino = false
+    } else {
+        dino = sprites.create(img`
+            ........................
+            ........................
+            ..........ccc...........
+            .........cccc...........
+            .....ccccccc..ccc.......
+            ...cc555555cccccc.......
+            ..c5555555555bcc........
+            .c555555555555b..cc.....
+            c555551ff555555bccc.....
+            c55d55ff55555555bc......
+            c5555555555555555b......
+            .cbb31bb5555dd555b.cc...
+            .c5333b555ddddd55dccc...
+            .c533b55ddddddddddb.....
+            .c5555dddbb55bdddddccc..
+            ..ccccbbbb555bdddddccc..
+            ...cdcbc5555bddddddcc...
+            ....ccbc55bc5ddddddbcccc
+            .....cbbcc5555dddddddddc
+            .....ccbbb555ddbddddddc.
+            .....cdcbc55ddbbbdddcc..
+            ...ccdddccddddbcbbcc....
+            ...ccccccd555ddccc......
+            ........cccccccc........
+            `, SpriteKind.Enemy)
+        fightDino = true
+        fightGhost = false
+    }
+}
 function enemyScene () {
     scene.setBackgroundImage(img`
         9999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999999966666699969999999999999999999999999999999999999999999999999999
@@ -133,6 +227,7 @@ function enemyScene () {
         4ddddddd4d444dd4dd4ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd4ddddddddddddddddddddddddddddddd4ddddddddddddddddddddddddddddd4ddddddddd
         dddddddddd444ddddd3ddddddddddddd4ddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddddd44dddddddddddddddddddddddddddddddddd4ddddddddd
         `)
+    newEnemy()
 }
 function safeScene () {
     scene.setBackgroundImage(img`
@@ -275,7 +370,7 @@ function safeScene () {
         . . . f f f f f f f f f . . . . 
         . . . . . . . . . . . . . . . . 
         `, SpriteKind.Portal)
-    portal.setPosition(135, 100)
+    portal.setPosition(135, 110)
     guide = sprites.create(img`
         . . . . . . . c c c . . . . . . 
         . . . . . . c b 5 c . . . . . . 
@@ -294,14 +389,31 @@ function safeScene () {
         . . . . c b 5 5 5 5 b c . . . . 
         . . . . . f f f f f f . . . . . 
         `, SpriteKind.NPC)
-    guide.setPosition(50, 100)
-    guide.sayText("Kill 10 enemies")
+    guide.setPosition(50, 110)
+    fightGhost = false
+    fightDino = false
 }
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Enemy, function (sprite, otherSprite) {
+    if (swordOut) {
+        otherSprite.destroy()
+        fightGhost = false
+        info.changeScoreBy(1)
+        newEnemy()
+        projectile.destroy()
+    }
+})
+let projectile: Sprite = null
 let portal: Sprite = null
+let dino: Sprite = null
+let fightDino = false
+let fightGhost = false
+let ghost: Sprite = null
 let guide: Sprite = null
+let swordOut = false
 let canAttack = false
+let hero: Sprite = null
 safeScene()
-let hero = sprites.create(img`
+hero = sprites.create(img`
     . . . . . . f f f f . . . . . . 
     . . . . f f f 2 2 f f f . . . . 
     . . . f f f 2 2 2 2 f f f . . . 
@@ -319,14 +431,17 @@ let hero = sprites.create(img`
     . . . . . f f f f f f . . . . . 
     . . . . . f f . . f f . . . . . 
     `, SpriteKind.Player)
-controller.moveSprite(hero, 100, 0)
-hero.setPosition(100, 100)
+controller.moveSprite(hero, 75, 0)
+hero.setPosition(100, 110)
 hero.setStayInScreen(true)
+hero.ay = 120
 game.splash("Hello traveler")
 game.splash("You must defeat enemies")
-game.splash("to rank up")
+game.splash("To rank up")
 canAttack = false
-let swordOut = false
+swordOut = false
+info.setLife(3)
+guide.sayText("Kill 10 enemies")
 game.onUpdate(function () {
     if (controller.right.isPressed()) {
         hero.setImage(img`
@@ -390,15 +505,55 @@ game.onUpdate(function () {
             ...fffffeeee.ccccc......
             ..ffffffff...c..........
             ..fff..ff...............
-            ........................
-            ........................
-            ........................
-            ........................
-            ........................
-            ........................
-            ........................
-            ........................
             `)
         swordOut = true
+    }
+    if (info.score() == 10) {
+        dino.destroy()
+        ghost.destroy()
+        safeScene()
+        info.changeScoreBy(1)
+    }
+})
+game.onUpdateInterval(4000, function () {
+    if (fightGhost) {
+        projectile = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . 7 9 7 . . . 7 7 9 9 . 
+            . . . . 7 9 9 7 7 . 9 7 . . . . 
+            . . . . 7 7 7 7 9 . . . . . . . 
+            . . . . 9 9 7 9 7 . 7 7 9 7 . . 
+            . . . . . 9 7 7 . . . . . 9 7 9 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, ghost, randint(-50, -60), 0)
+    }
+    if (fightDino) {
+        projectile = sprites.createProjectileFromSprite(img`
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . 2 3 3 3 2 2 2 . . . . . 
+            . . . 2 3 1 1 1 1 1 3 3 2 2 2 . 
+            . . . 2 1 1 1 1 1 1 1 1 1 1 1 . 
+            . . . 2 3 1 1 1 1 1 3 3 2 2 2 . 
+            . . . . 2 3 3 3 3 2 2 . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            . . . . . . . . . . . . . . . . 
+            `, dino, randint(-50, -60), 0)
     }
 })
